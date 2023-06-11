2993ca63ec4fd528137dac7f87c57844c8360d6a
Numan Ali <numan4599322ali@gmail.com>
2023-06-11 12:17:03 +0530

    final

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+export const metadata = {
+  title: "findIt",
+  description: "Discover & Share Data",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="eng">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
